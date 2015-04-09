d4ccd10be53b96125b347b9e3a4d323e5b081baf
hanbyul-here <hanbyul@nyu.edu>
2015-04-09 17:11:43 -0400

    change links dependin on staging/production

---
+++ _plugins/environment_variables.rb
@@ -0,0 +1,26 @@
+
+
+# Plugin to add environment variables to the `site` object in Liquid templates
+ 
+module Jekyll
+ 
+  class EnvironmentVariablesGenerator < Generator
+ 
+    def generate(site)
+      site.config['env'] = ENV['TARGET'] || 'dev'
+      # Add other environment variables to `site.config` here...
+      if site.config['env'] == 'prod'
+        site.config['url'] = '//transit.land/'
+        site.config['playground_url'] = '//playground.transit.land/'
+      elsif site.config['env'] == 'stage'
+        site.config['url'] = '//staging.transit.land/'
+        site.config['playground_url'] = '//staging.playground.transit.land/'
+      else
+        site.config['url'] = '/'
+        site.config['playground_url'] = '/'
+      end
+    end
+ 
+  end
+ 
+end
